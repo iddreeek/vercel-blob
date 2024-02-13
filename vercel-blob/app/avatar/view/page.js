ead37d95ed90b6aa13e1726061295c6559a396a9
iddreeek <iddreeek@gmail.com>
2024-02-14 06:59:01 +0800

    c3

--- vercel-blob/app/avatar/view/page.js
+++ vercel-blob/app/avatar/view/page.js
@@ -7,8 +7,8 @@ export default async function Page() {
     <>
       {response.blobs.map((blob) => (
         <div key={blob.pathname}>
-        {blob.pathname}
-        <Image src={blob.url} width={50} height={50}></Image>
+          {blob.pathname}
+          <Image src={blob.url} width={50} height={50}></Image>
         </div>
       ))}
     </>
